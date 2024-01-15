6e5bf7c2d836651060702cf5e902c1bdfe016219
Levi Hutchins <levihutchins0@gmail.com>
2024-01-16 09:31:26 +1100

    frontend init

--- backend/phishing_api.py
+++ backend/phishing_api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pysafebrowsing import SafeBrowsing
 from fastapi.responses import JSONResponse
 
 import models
@@ -12,7 +13,19 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/link_prediction/")
-async def create_item(link: models.PhishingLink):
+async def inhouse_model_prediction(link: models.PhishingLink):
     x_labels = url_processing.get_url_prediction_values(link.url_link)
     prediction = model_prediction.make_prediction(x_labels)
-    return JSONResponse(int(prediction[0]))
+    if prediction[0] == -1: return JSONResponse("phishing")
+    elif prediction[0] == 0: return JSONResponse("suspicious")
+    else: return JSONResponse("legitimate")
+
+@app.post("/virus_total_analysis/")
+async def virus_total_api():
+    print()
+
+
+
+
+
+
