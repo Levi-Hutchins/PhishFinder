b10515a6a265829a68eb4401e1cf6dcae82f0667
Levi Hutchins <levihutchins0@gmail.com>
2024-01-22 13:52:39 +1100

    test

--- backend/scripts/config/paths_config.py
+++ backend/scripts/config/paths_config.py
@@ -2,3 +2,4 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA = os.path.join(BASE_DIR, "../../dataset", "dataset.csv")
 MODEL_PATH = os.path.join(BASE_DIR, "../onnx", "model.onnx")
+
