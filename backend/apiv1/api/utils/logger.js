3d26b7657cea50269776ad2da019b0e1597969b5
Levi Hutchins <levihutchins0@gmail.com>
2024-05-29 20:47:35 +1000

    sources for future dev

--- backend/apiv1/api/utils/logger.js
+++ backend/apiv1/api/utils/logger.js
@@ -16,3 +16,8 @@ log.add(
 
 
 module.exports = log
+
+
+// Transporting logs
+// https://betterstack.com/community/guides/logging/how-to-install-setup-and-use-winston-and-morgan-to-log-node-js-applications/
+// https://www.npmjs.com/package/winston-mongodb
