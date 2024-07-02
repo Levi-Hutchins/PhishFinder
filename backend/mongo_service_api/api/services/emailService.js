cafb48d6eba7332eb335d44358700d99cb53d76c
Levi Hutchins <levihutchins0@gmail.com>
2024-07-02 10:41:59 +1000

    simple input sanitation before db query

--- backend/mongo_service_api/api/services/emailService.js
+++ backend/mongo_service_api/api/services/emailService.js
@@ -1,5 +1,6 @@
 const Email = require('../models/Email');
+const validator = require('validator')
 
 exports.findAllEmails = () => Email.find({});
-exports.findEmail = (emailText) => Email.findOne({email_text: emailText});
+exports.findEmail = (emailText) => Email.findOne({email_text: validator.escape(emailText)});
 exports.createEmail = (emailData) => Email.create(emailData);
