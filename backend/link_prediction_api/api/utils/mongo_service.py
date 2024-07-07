a540923b9acc3b3027a9353409865f0bdd608b07
Levi Hutchins <levihutchins0@gmail.com>
2024-07-07 21:11:44 +1000

    fixxxxxx

--- backend/link_prediction_api/api/utils/mongo_service.py
+++ backend/link_prediction_api/api/utils/mongo_service.py
@@ -1,4 +1,10 @@
 from connect_db import get_mongo_client
-
+from pymongo import MongoClient
 def insert_cloudflare_scan():
-    
+    client: MongoClient = get_mongo_client()
+    print(client.get_database("PhishFinder").list_collection_names())
+
+
+
+
+insert_cloudflare_scan()
