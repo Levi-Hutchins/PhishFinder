7807d27a4c11e63ce64a259624d6d6d8e999340f
Levi Hutchins <levihutchins0@gmail.com>
2024-07-07 21:17:11 +1000

    pipeline fix

--- backend/link_prediction_api/api/utils/mongo_service.py
+++ backend/link_prediction_api/api/utils/mongo_service.py
@@ -2,7 +2,6 @@ from connect_db import get_mongo_client
 from pymongo import MongoClient
 def insert_cloudflare_scan():
     client: MongoClient = get_mongo_client()
-    print(client.get_database("PhishFinder").list_collection_names())
 
 
 
